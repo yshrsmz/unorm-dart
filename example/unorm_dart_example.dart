9f2fafa9f76fe77dcc6afc8e903546bae0beb24b
yshrsmz <the.phantom.bane@gmail.com>
2018-08-13 19:16:51 +0900

    add project skelton

---
+++ example/unorm_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:unorm_dart/unorm_dart.dart';
+
+main() {
+  var awesome = new Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/unorm_dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library unorm_dart;
+
+export 'src/unorm_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
